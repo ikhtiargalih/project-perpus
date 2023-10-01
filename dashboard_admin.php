368dbac699be546c7d8f7fded88fe614cf449c87
Galih Ikhtiar <galihikhtiar10@gmail.com>
2023-10-01 21:07:38 +0700

    styling media datatable

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -15,7 +15,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="assets/admin/vendors/styles/core.css">
     <link rel="stylesheet" type="text/css" href="assets/admin/vendors/styles/icon-font.min.css">
@@ -24,7 +25,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="assets/admin/vendors/styles/style.css">
     <!-- cdn jquery -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
@@ -175,43 +177,57 @@
                 <h4 class="weight-600 font-18 pb-10">Menu Dropdown Icon</h4>
                 <div class="sidebar-radio-group pb-10 mb-10">
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebaricon-1" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-1" checked="">
+                        <input type="radio" id="sidebaricon-1" name="menu-dropdown-icon" class="custom-control-input"
+                            value="icon-style-1" checked="">
                         <label class="custom-control-label" for="sidebaricon-1"><i class="fa fa-angle-down"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebaricon-2" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-2">
+                        <input type="radio" id="sidebaricon-2" name="menu-dropdown-icon" class="custom-control-input"
+                            value="icon-style-2">
                         <label class="custom-control-label" for="sidebaricon-2"><i class="ion-plus-round"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebaricon-3" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-3">
-                        <label class="custom-control-label" for="sidebaricon-3"><i class="fa fa-angle-double-right"></i></label>
+                        <input type="radio" id="sidebaricon-3" name="menu-dropdown-icon" class="custom-control-input"
+                            value="icon-style-3">
+                        <label class="custom-control-label" for="sidebaricon-3"><i
+                                class="fa fa-angle-double-right"></i></label>
                     </div>
                 </div>
 
                 <h4 class="weight-600 font-18 pb-10">Menu List Icon</h4>
                 <div class="sidebar-radio-group pb-30 mb-10">
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebariconlist-1" name="menu-list-icon" class="custom-control-input" value="icon-list-style-1" checked="">
-                        <label class="custom-control-label" for="sidebariconlist-1"><i class="ion-minus-round"></i></label>
+                        <input type="radio" id="sidebariconlist-1" name="menu-list-icon" class="custom-control-input"
+                            value="icon-list-style-1" checked="">
+                        <label class="custom-control-label" for="sidebariconlist-1"><i
+                                class="ion-minus-round"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebariconlist-2" name="menu-list-icon" class="custom-control-input" value="icon-list-style-2">
-                        <label class="custom-control-label" for="sidebariconlist-2"><i class="fa fa-circle-o" aria-hidden="true"></i></label>
+                        <input type="radio" id="sidebariconlist-2" name="menu-list-icon" class="custom-control-input"
+                            value="icon-list-style-2">
+                        <label class="custom-control-label" for="sidebariconlist-2"><i class="fa fa-circle-o"
+                                aria-hidden="true"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebariconlist-3" name="menu-list-icon" class="custom-control-input" value="icon-list-style-3">
+                        <input type="radio" id="sidebariconlist-3" name="menu-list-icon" class="custom-control-input"
+                            value="icon-list-style-3">
                         <label class="custom-control-label" for="sidebariconlist-3"><i class="dw dw-check"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebariconlist-4" name="menu-list-icon" class="custom-control-input" value="icon-list-style-4" checked="">
-                        <label class="custom-control-label" for="sidebariconlist-4"><i class="icon-copy dw dw-next-2"></i></label>
+                        <input type="radio" id="sidebariconlist-4" name="menu-list-icon" class="custom-control-input"
+                            value="icon-list-style-4" checked="">
+                        <label class="custom-control-label" for="sidebariconlist-4"><i
+                                class="icon-copy dw dw-next-2"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebariconlist-5" name="menu-list-icon" class="custom-control-input" value="icon-list-style-5">
-                        <label class="custom-control-label" for="sidebariconlist-5"><i class="dw dw-fast-forward-1"></i></label>
+                        <input type="radio" id="sidebariconlist-5" name="menu-list-icon" class="custom-control-input"
+                            value="icon-list-style-5">
+                        <label class="custom-control-label" for="sidebariconlist-5"><i
+                                class="dw dw-fast-forward-1"></i></label>
                     </div>
                     <div class="custom-control custom-radio custom-control-inline">
-                        <input type="radio" id="sidebariconlist-6" name="menu-list-icon" class="custom-control-input" value="icon-list-style-6">
+                        <input type="radio" id="sidebariconlist-6" name="menu-list-icon" class="custom-control-input"
+                            value="icon-list-style-6">
                         <label class="custom-control-label" for="sidebariconlist-6"><i class="dw dw-next"></i></label>
                     </div>
                 </div>
@@ -292,22 +308,31 @@
 
                 </div>
             </div>
+
+            <style>
+                @media (max-width: 768px) {
+                    .card-box {
+                        flex: 0 0 100%;
+                        position: absolute;
+                    }
+                }
+            </style>
+
             <!-- table buku baru -->
             <?php
             include 'config/koneksi.php';
             $query = mysqli_query($koneksi, "SELECT * FROM book ORDER BY id_buku DESC");
             ?>
-            <div class="card-box mb-5">
-                <div class="container p-4">
-                <a href="action/create_buku.php" class="d-flex justify-content-between">
-                    <h2>Buku Baru</h2>
-                    <button type="button" class="btn btn-outline-dark bi-plus-lg"></button>    
+            <div class="card-box" >
+                <a href="action/create_buku.php" class="d-flex justify-content-between pd-20">
+                <h2>Daftar Buku Baru</h2>
+                    <button type="button" class="btn btn-outline-dark bi-plus-lg"></button>
                 </a>
                 <table class="data-table table nowrap mb-5">
                     <thead>
                         <tr>
-                            <th>No</th>
-                            <th class="table-plus datatable-nosort">Sampul</th>
+                            <th class="table-plus datatable-nosort">No</th>
+                            <th>Sampul</th>
                             <th>Judul</th>
                             <th>Kode</th>
                             <th>Nomor Buku</th>
@@ -322,39 +347,43 @@
                         while ($data = mysqli_fetch_array($query)) {
                             $no++
                         ?>
-                            <tr>
-                            <tr>
-                                <td><?= $no ?></td>
-                                <td class="table-plus">
-                                    <img src="assets/img/<?= $data['foto_buku'] ?>" width="70" height="70" alt="">
-                                </td>
-                                <td>
-                                    <h5 class="font-16"><?= $data['judul_buku'] ?></h5>
-                                    <?= $data['penerbit'] ?>
-                                </td>
-                                <td><?= $data['kode_buku'] ?></td>
-                                <td><?= $data['no_urut'] ?></td>
-                                <td><?= $data['penerbit'] ?></td>
-                                <td><?= $data['keterangan'] ?></td>
-                                <td>
-                                    <div class="dropdown">
-                                        <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
-                                            <i class="dw dw-more"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                                            <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
-                                            <a class="dropdown-item " href="" onclick="ubahData('<?= $data['id_buku'] ?>')" data-toggle="modal" data-target="#myModal"><i class="dw dw-edit2"></i> Edit</a>
-                                            <a class="dropdown-item" href="controllers/delete_data_buku.php?id_buku=<?= $data['id_buku'] ?>"><i class="dw dw-delete-3"></i> Delete</a>
-                                        </div>
+                        <tr>
+                        <tr>
+                            <td><?= $no ?></td>
+                            <td class="table-plus">
+                                <img src="assets/img/<?= $data['foto_buku'] ?>" width="70" height="70" alt="">
+                            </td>
+                            <td>
+                                <h5 class="font-16"><?= $data['judul_buku'] ?></h5>
+                                <?= $data['penerbit'] ?>
+                            </td>
+                            <td><?= $data['kode_buku'] ?></td>
+                            <td><?= $data['no_urut'] ?></td>
+                            <td><?= $data['penerbit'] ?></td>
+                            <td><?= $data['keterangan'] ?></td>
+                            <td>
+                                <div class="dropdown">
+                                    <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#"
+                                        role="button" data-toggle="dropdown">
+                                        <i class="dw dw-more"></i>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                        <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                        <a class="dropdown-item " href="" onclick="ubahData('<?= $data['id_buku'] ?>')"
+                                            data-toggle="modal" data-target="#myModal"><i class="dw dw-edit2"></i>
+                                            Edit</a>
+                                        <a class="dropdown-item"
+                                            href="controllers/delete_data_buku.php?id_buku=<?= $data['id_buku'] ?>"><i
+                                                class="dw dw-delete-3"></i> Delete</a>
                                     </div>
-                                </td>
-                            </tr>
+                                </div>
+                            </td>
+                        </tr>
                         <?php
                         }
                         ?>
                     </tbody>
                 </table>
-                </div>
             </div>
         </div>
     </div>
@@ -375,7 +404,7 @@
 
             $.post(url, {
                 id_buku: a
-            }, function(data) {
+            }, function (data) {
                 $('.modal-title').html('Update');
                 $('.modal-body').html(data);
 
